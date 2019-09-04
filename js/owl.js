a093c72a2bedbbd80b20b16c80078c06dd16b7ea
CHRIS KOZEKWA <chriskozekwa04@gmail.com>
2019-09-03 20:47:13 -0500

    Add files via upload

---
+++ js/owl.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $('#customers').owlCarousel({
+        loop: true,
+        margin: 10,
+        smartSpeed: 4000,
+        autoplay: 8000,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    })
+
+    //owl carousel
+    $(".owl-prev").html('<i class="fas fa-chevron-left fa-2x"></i>');
+    $(".owl-next").html('<i class="fas fa-chevron-right fa-2x"></i>');
+
+});
